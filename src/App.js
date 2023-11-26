86c236175289999780efecb1929eadc1bf007abe
arief-wicaksono <mailtriwicaksono@gmail.com>
2023-11-26 21:16:28 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import classnames from "classnames";
+
+import shoppingIcon from "./assets/shopping-icon.svg";
+import plusIcon from "./assets/plus-icon.svg";
+import minusIcon from "./assets/minus-icon.svg";
+
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState([
+    { title: "Susu Ultra", count: 1 },
+    { title: "Tempe Mendoan", count: 1 },
+    { title: "Melon", count: 1 },
+  ]);
+
+  const handleSubmit = () => {};
+  const handleAdditionCount = (index) => {
+    const newTodos = [...todos];
+
+    newTodos[index].count = newTodos[index].count + 1;
+
+    setTodos(newTodos);
+  };
+
+  const handleSubstractionCount = (index) => {
+    const newTodos = [...todos];
+
+    newTodos[index].count = newTodos[index].count - 1;
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="nav">
+        <img className="nav-icon" src={shoppingIcon} alt="shopping icon" />
+        <h1 className="nav-title">Shopping List</h1>
+      </nav>
+
+      <section className="container">
+        <form className="form">
+          <input
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+            value={value}
+            className="input"
+            type="text"
+            placeholder="list"
+          />
+          <button className="add-button" type="submit">
+            Add
+          </button>
+        </form>
+
+        {todos.length > 0 ? (
+          <div className="todos">
+            {todos.map((todo, index, arr) => {
+              return (
+                <div
+                  key={index}
+                  className={`todo ${
+                    !(arr.length === index + 1) && "todo-divider"
+                  }`}
+                >
+                  {todo.title}
+                  <div className="todo-icon-wrapper">
+                    <div className="todo-count">{todo.count}</div>
+                    <button
+                      onClick={() => handleSubstractionCount(index)}
+                      className="todo-action-button"
+                    >
+                      <img src={minusIcon} alt="minus icon" />
+                    </button>
+                    <button
+                      onClick={() => handleAdditionCount(index)}
+                      className="todo-action-button"
+                    >
+                      <img src={plusIcon} alt="plus icon" />
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </section>
+    </>
   );
 }
 
